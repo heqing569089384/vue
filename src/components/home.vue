71605d6bd6f8d1fc4ebacf9c1d79e0699275414b
He Qing <1715047526@qq.com>
2019-06-04 11:28:24 +0800

    1.0.0

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <span>{{msg}}</span>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            msg:'我是Home组件'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ src/components/user.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>User</h1>
+        <p>我是user组件</p>
+        <p>我是User的动态部分{{userId}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            userId:``
+        }
+    },
+    watch:{
+        $route(to,from){// to表示的是你要去的那个组件，from 表示的是你从哪个组件过来的，它们是两个对象，你可以把它打印出来，它们也有一个param 属性
+        alert(111)
+            console.log(to);
+            console.log(from);
+            this.userId = to.params.id
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
+
